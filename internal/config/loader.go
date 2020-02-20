b3d9c6c770598c4e988e9cc034b037dfe0968ba4
Jaroslav Šmolík <grissius@gmail.com>
2020-02-20 19:11:25 +0100

    🔧 Add option to take port from env variable

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"os"
+	"strconv"
 )
 
 var Config struct {
@@ -10,8 +11,13 @@ var Config struct {
 }
 
 func LoadConfig() {
+	defaultPort := 8000
+	envPort, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		defaultPort = envPort
+	}
 	// COOL: having built in param parsing is just cool
-	port := flag.Int("port", 8000, "Local server port")
+	port := flag.Int("port", defaultPort, "Local server port")
 	help := flag.Bool("help", false, "Print help")
 	flag.Parse()
 	if *help == true {
